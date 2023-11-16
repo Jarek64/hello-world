1ac092aab7817a21d65dfa1ddf2994ed28b18458
jarek64 <jarekthejackal@gmail.com>
2023-11-15 20:04:38 -0700

    added SSH file

---
+++ terraform/ssh.tf
@@ -0,0 +1,6 @@
+resource "harvester_ssh_key" "ssh-key" {
+    name = "akunkee-at-kronos"
+    namespace = "default"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAz3MkPWjNlrkK+2KPlKxnpHSGmm231U0hb+WKC132Paj1EXzLOUurRGMyRXuDm1PyiirQKWny61BHlxRACx8cqjEcP7vmPm0AYu98C1dpTCH6bUTPv4Z+aE4tvwx4JOY5M5M3Oic25XxPGINaS7Pez+fEqYyqeCAngUiUrMV9BbS+mWIAa4ia+VJJxumvl8j2FgW+x5rtQlYLtqNh17trStkBMRPlDucPrqLYkbJvQQicuSFFMSNaO/85fB+VaqS3VWSTVWcvv86u4ZkD9Ld/jLhoBZ/4b03IFKs2YclOMs+IryQf14IyLBklQo762bTfPl9zgMsUH6KbX9hTKtOjgw== akunkee@kronos"
+
+}
